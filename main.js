707dc5790428a97a750da835ba8182277b60c904
santiag0ur <83630273+santiag0ur@users.noreply.github.com>
2022-01-11 16:45:29 +0100

    l4 and l5 soludions completed

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
-      url: 'https://www.google.com'
+      inventory: 0,
+      onSale: false,
+      details: ['50% cotton', '30% wool', '20%polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ],
+      sizes: ['XL', 'L', 'M', 'S', 'XS']
     };
   }
 });
